88ab858a3a6c838803c492d0b956b7cf25162786
mohitm123 <35539313+mohitm123@users.noreply.github.com>
2018-12-30 16:10:27 +0530

    Create Vanya_and_Lanterns.cpp

---
+++ Vanya_and_Lanterns.cpp
@@ -0,0 +1,27 @@
+//SOLN BY MOHIT
+//contest: Codeforces Round #280 (Div. 2), problem: (B) Vanya and Lanterns, Happy New Year!, #
+ 
+ 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, l, r;
+    cin >> n >> l;
+    int p[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> p[i];
+
+    sort(p, p + n);
+    r = max(p[0], l - p[n - 1]) * 2;
+
+    for (int i = 0; i < n - 1; i++)
+        r = max(r, p[i + 1] - p[i]);
+
+    cout.precision(20);
+    cout << fixed << r / 2.0 << endl;
+    return 0;
+}
