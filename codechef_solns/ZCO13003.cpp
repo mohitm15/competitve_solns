546d37325edfd27c361eb66c40635ca30bb301d3
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-01-31 00:41:55 +0530

    ZCO Practice Contest

--- codechef_solns/ZCO13003.cpp
+++ codechef_solns/ZCO13003.cpp
@@ -1,20 +1,53 @@
-#include <stdio.h>
+//By Mohit Maroliya
+//Chewing.cpp  :ZCO Practice Contest
+
+#include<iostream>
+#include <cmath>  
+#include<cstdio>
+#include <algorithm>
+#include <vector>
+#define gc getchar
+using namespace std;
+void scanint(long long &x)
+{
+    register int c = gc();
+    x = 0;
+    for(;(c<48 || c>57);c = gc());
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+}
+
 int main()
 {
-	long long int n,k;
-	scanf("%lld%lld",&n,&k);
-	long long int a[n];
-	for (long long int i=0;i<n;i++)
-		scanf("%lld",&a[i]);
-	long long int count=0;
-	for (long long int i=0;i<n-1;i++)
+	long long N,K;
+	scanint(N);
+	scanint(K);
+	long hardness[N];
+	long long i,j,tmp,c=0;
+	long long pairCount = 0;
+
+	for(i=0;i<N;++i)
 	{
-		for (long long int j=i+1;j<n;j++)
+		scanint(tmp);
+		if(tmp<K)
 		{
-			if (a[i]+a[j]<k)
-				count++;
-		}
+			hardness[c]=tmp;
+			c++;
+		}		
 	}
-	printf("%lld\n",count);
+	sort(hardness,hardness+c);
+ 	for(i=0,j=c-1;i<j; )
+    {
+        
+		if( hardness[i]+hardness[j]<K)
+        {
+         	pairCount += j-i;
+            i++;
+        }
+        else
+        {
+            j--;
+        }
+ 	}
+   	cout<<pairCount;
 	return 0;
 }
