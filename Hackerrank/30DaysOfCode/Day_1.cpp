09cc4c6c613f0c9567d8646f9b87b4b3721836b9
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-02-11 23:16:53 +0530

    Create Day_1.cpp

---
+++ Hackerrank/30DaysOfCode/Day_1.cpp
@@ -0,0 +1,28 @@
+//DAY 1
+
+  int j;
+    double dd;
+    string str,t;
+    cin>>j>>dd;
+    std::getline(cin,t);
+    std::getline(cin,str);
+    t=s+str;
+    cout<<i+j<<'\n';
+    if(dd==4.0)
+     cout<<d+dd<<".0"<<'\n';
+     else
+     cout<<d+dd<<'\n';
+      cout<<t<<'\n';
+    
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
