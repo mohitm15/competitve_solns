bf6499f1070bb2c662263dcb34f7d93abec68231
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-02-25 12:40:11 +0530

    Create Day_16.cpp

---
+++ Hackerrank/30DaysOfCode/Day_16.cpp
@@ -0,0 +1,43 @@
+//DAY 16
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    try{
+    int n;
+    n=stoi(S);
+    cout<<n<<endl;
+    }
+    catch(exception a)
+    {
+        cout<<"Bad String"<<endl;
+    }
+    return 0;
+}
+
