618b20c2e0a15c10634791d139c2a780c05ff524
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-03-03 22:56:49 +0530

    Create Day_21.cpp

---
+++ Hackerrank/30DaysOfCode/Day_21.cpp
@@ -0,0 +1,46 @@
+//DAY 21
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+// I will use template here
+template <typename t>
+void printArray(vector<t> vec)
+{
+    for (auto &element : vec)
+    cout<<element<<'\n';
+}
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
+}
