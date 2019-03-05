5f9bc7e66b39602ae8d3897c61240518dc29b012
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-03-05 18:59:20 +0530

    Create Day_22.cpp

---
+++ Hackerrank/30DaysOfCode/Day_22.cpp
@@ -0,0 +1,77 @@
+//DAY 22
+
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+		int getHeight(Node* root){
+          //Write your code here
+          int cnt=0;
+          if(!root)
+        {
+            return -1;
+        }
+        int leftd=getHeight(root->left);
+        int rightd=getHeight(root->right);
+        if(leftd>rightd)
+            return leftd +1;
+        else
+            return rightd+1;
+        }
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
+    
+       
+            
+        
