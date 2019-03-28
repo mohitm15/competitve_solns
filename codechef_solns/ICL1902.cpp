da9126bbb84064e6efefd0a0f0712d8f52c2d398
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-03-29 00:25:19 +0530

    International Coding League 2019 -Problem 1

--- codechef_solns/ICL1902.cpp
+++ codechef_solns/ICL1902.cpp
@@ -2,36 +2,33 @@
 
 //By Mohit Maroliya
 
-#include<iostream>
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-  int n,m;
-  int a[31]={961,900,841,784,729,676,625,576,529,484,441,400,361,324,289,256,225,196,169,144,121,100,81,64,49,36,25,16,9,4,1};
-  std::cin >> n;
-  m=n;
-  int cnt=1;
-  for(int i=0;i<30;i++)
-  {
-    if(m>a[i])
-    {
-      m=m-a[i];
-      cnt++;
-      //std::cout << "yo" << '\n';
-    }
-    else if(n==a[i])
-    {
-       cnt=1;
-      break;
-    }
-    else if(m==a[i])
-    {
-      // cnt++;
-      break;
-    }
-  }
-  //if(n==0)
-  std::cout << cnt << '\n';
-  return 0;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+		int n,c=0;
+		cin>>n;
+		while(n!=0)
+		{
+			float k = (sqrt(n));
+			k=round(k);
+			//cout<<k;
+			if((k*k)>n)
+				k=k-1;
+			n = n-(k*k);
+			//cout<<n;
+			c++;
+		}
+		cout<<c<<"\n";
+	}
+	return 0;
 }
+
+
+//Earlier solution was time consuming so latest soln above:
