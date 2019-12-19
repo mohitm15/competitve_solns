26d0ccf87d46ad5dc3a363adcc8b98f6739fde66
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-19 17:03:55 +0530

    Codeforces Technocup Elimination #4, problem: (C)

---
+++ codeforces/As_simple_as_one_and_two.cpp
@@ -0,0 +1,51 @@
+// contest: Codeforces Round #606 (Div. 2, based on Technocup 2020 Elimination Round 4), problem: (C) As Simple as One and Two, Accepted, #
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        vector<int> v;
+        for(string t:{"twone","one","two"})
+        {
+            for(size_t pos =0;(pos = s.find(t,pos))!=string::npos;)
+            {
+                s[pos+t.length()/2]= '?';
+                v.push_back(pos+t.length()/2);
+            }
+        }
+        cout<<v.size()<<endl;
+        for(auto vv:v)
+        cout<<vv+1<<" ";
+        next;
+    }
+    return 0;
+}
