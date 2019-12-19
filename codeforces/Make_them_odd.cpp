e406540409b45ed9528e51d050b2a11edde4b45b
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-19 16:30:14 +0530

    Technocup Elimination Round #4 , problem (B)

---
+++ codeforces/Make_them_odd.cpp
@@ -0,0 +1,57 @@
+// Contest : Technocup Elimination Round #4 , problem (B): Make Them Odd, Accepted #
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        set<int> a;
+        for(int i=0;i<n;i++)
+        {
+            int elem;
+            cin>>elem;
+            a.insert(elem);
+        }
+        int res=0;
+        while(!a.empty())
+        {
+            int m = *a.rbegin();
+            a.erase(m);
+            if(m%2==0)
+            {
+                res++;
+                a.insert(m/2);
+            }
+        }
+        cout<<res<<endl;
+    }
+    return 0;
+}
