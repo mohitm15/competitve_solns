9487680930d869a716477710ba6b4daab5bccf25
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-21 20:20:48 +0530

    Codeforces Round #609 (Div. 2), problem: (A)

---
+++ codeforces/Equation.cpp
@@ -0,0 +1,60 @@
+// contest: Codeforces Round #609 (Div. 2), problem: (A) Equation, Accepted, #
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+ 
+bool isComposite(int n) 
+{ 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return false; 
+   
+    if (n%2 == 0 || n%3 == 0) return true; 
+   
+    for (int i=5; i*i<=n; i=i+6) 
+        if (n%i == 0 || n%(i+2) == 0) 
+           return true; 
+   
+    return false; 
+}
+ 
+ 
+int main()
+{
+	int n;
+	cin>>n;
+	int i=4;
+	while(1)
+	{
+		if(isComposite(i)==true && isComposite(i+n)==true)
+		{
+			cout<<i+n<<" "<<i<<endl;
+			break;
+		}
+		else
+		{
+			i++;
+		}
+		
+	}
+	return 0;
+}
