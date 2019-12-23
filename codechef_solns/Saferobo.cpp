8062d460d9bfd825b4cdcfc5e1a1663677a1a26d
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-23 18:35:25 +0530

    ICPC Asia-Gwalior- Pune Onsite Contest 2019 : Q 1

---
+++ codechef_solns/Saferobo.cpp
@@ -0,0 +1,54 @@
+// ICPC Asia-Gwalior- Pune Onsite Replay Contest 2019 : Problem 1
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int sa,sb,aindex,bindex;
+        cin>>sa>>sb;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='A')
+            aindex = i;
+            else if(s[i]=='B')
+            bindex = i;
+        }
+        bool flag =false;
+        for(int i=0;i<=s.length()/2;i++)
+        {
+            aindex = sa + aindex;
+            bindex = bindex -sb;
+            if(aindex == bindex)
+            {
+                flag = true;
+                break;
+            }
+            flag =false;
+        }
+        if(flag)
+        cout<<"unsafe"<<endl;
+        else
+        cout<<"safe"<<endl;
+    }
+    return 0;
+}
