0b2507e6f91e4d6db2f5093fe9ee3dd4a8134967
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-28 13:05:54 +0530

    Educational Codeforces Round 79 ,problem: (A)

---
+++ New_Year_Garland.cpp
@@ -0,0 +1,41 @@
+// contest: Educational Codeforces Round 79 (Rated for Div. 2), problem: (A) New Year Garland, Happy New Year!, #
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+
+
+
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+	{
+        vector<int> v(3);
+		cin>>v[0]>>v[1]>>v[2];
+        sort(v.begin(),v.end());
+        if((v[2]-1)<=(v[0]+v[1]))
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+	}
+	return 0;
+}
