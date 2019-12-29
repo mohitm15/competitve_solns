2676712be4319b419021c7c3df6b47652239f5bb
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-29 19:32:21 +0530

    Codeforces Round #611 (Div. 3), problem: (B)

---
+++ codeforces/Candies_division.cpp
@@ -0,0 +1,44 @@
+// contest: Codeforces Round #611 (Div. 3), problem: (B) ,Candies Division, Accepted,
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+
+int mini(int a,int b)
+{
+    int res = a>b?b:a;
+    return res;
+}
+ 
+int main() 
+{	
+	int t;
+	cin >> t;
+	while(t--)
+    {
+		int n, k;
+		cin >> n >> k;
+		int ans = n - n % k;
+		ans += mini(n % k, k / 2);
+		cout<<ans<< endl;
+	}	
+	return 0;
+}
