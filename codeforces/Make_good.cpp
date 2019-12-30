c6d5776241070a4587db3ab893b85f7ca8b6016d
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-30 16:57:51 +0530

    Good Bye 2019, problem: (C)

---
+++ codeforces/Make_good.cpp
@@ -0,0 +1,52 @@
+// contest: Good Bye 2019, problem: (C) Make Good, Happy New Year!, #
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        ll s=0,x=0;
+        cin>>n;
+        vector<int> a(n);
+        f0(i,n) 
+        {
+            cin>>a[i];
+            s+=a[i];
+            x^=a[i];
+        }
+        if(s==(2*x))
+        cout<<"0"<<endl<<endl;
+        else
+        {
+            cout<<2<<endl;
+            cout<<x<<" "<<s+x<<endl;
+        }
+        
+ 
+    }
+    return 0;
+}
