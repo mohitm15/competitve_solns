2f0dbc17f755913fc63c9af008087cee5df8f51a
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-12-31 15:52:12 +0530

    ICPC Amritapuri Onsite Replay Contest 2019 :  1

---
+++ codechef_solns/USANBOLT.cpp
@@ -0,0 +1,44 @@
+// ICPC Asia-Amritapuri Onsite Replay Contest 2019 : Problem 1
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll timeb=0,timet=0;
+        ll f,d,a,v;
+        cin>>f>>d>>a>>v;
+        timeb = a*f*f;
+        timet = 2*(f+d)*v*v;
+        // cout<<"bolt = "<<timeb<<endl;
+        // cout<<"tiger = "<<timet<<endl;
+        if(timet<=timeb)
+        cout<<"Tiger"<<endl;
+        else
+        cout<<"Bolt"<<endl;
+    }
+    return 0;
+}
