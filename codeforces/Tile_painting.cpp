9176fa234f97421661a061e9e4a0ac9c7679b2ea
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-08 00:19:36 +0530

    Codeforces Round #599 (Div. 2), problem: (C)

---
+++ codeforces/Tile_painting.cpp
@@ -0,0 +1,46 @@
+// contest: Codeforces Round #599 (Div. 2), problem: (C) Tile Painting, Happy New Year!, #
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+ 
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ 
+ 
+int maxi(int a,int b)
+{
+	int ans = a>=b?ans=a:ans=b;
+	return ans;
+}
+ 
+int main()
+{
+	ll n;cin>>n;
+	ll ans=n;
+	for(ll i=2;i<=sqrt(n);i++)
+	{
+		if(n%i==0)
+		{
+			ans = __gcd(i,ans);
+			ans = __gcd(n/i,ans);
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
+}
