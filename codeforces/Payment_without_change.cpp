610e8ad36dcea88b5ff5296547197cea83dbdf11
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-08 23:18:07 +0530

    Codeforces Round #598 (Div. 3), problem: (A)

---
+++ codeforces/Payment_without_change.cpp
@@ -0,0 +1,38 @@
+// contest: Codeforces Round #598 (Div. 3), problem: (A) Payment Without Change, Happy New Year!, #
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+ 
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+int main()
+{
+	int tt;
+	cin>>tt;
+	while(tt--)
+	{
+		ll a,b,n,s;
+		cin>>a>>b>>n>>s;
+		if(((s%n)<=b) && ((a*n+b)>=s))
+		cout<<"YES"<<endl;
+		else
+		cout<<"NO"<<endl;
+	}
+  return 0;
+}
