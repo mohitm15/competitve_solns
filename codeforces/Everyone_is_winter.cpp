a5bc7ed89e9676556df219476ff67175f28023c6
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-09 19:17:20 +0530

    Codeforces Round #603 (Div. 2), problem: (C)

---
+++ codeforces/Everyone_is_winter.cpp
@@ -0,0 +1,85 @@
+// contest: Codeforces Round #603 (Div. 2), problem: (C) Everyone is a Winner!, Happy New Year!, #
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n;j>0;j--)
+#define ll long long int
+ 
+using namespace std;
+ 
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ 
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int x=ceil(sqrt(n));
+		cout << x+n/x << endl;
+		f0(i,x) cout << i << " ";
+		rf(i,n/x) cout << n/i << " ";
+		cout <<  endl;
+	}
+	return 0;
+}
+
+// contest: Codeforces Round #603 (Div. 2), problem: (C) Everyone is a Winner!, Time limit exceeded on test 4, #
+#include<bits/stdc++.h>
+#define next cout<<endl
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+ 
+using namespace std;
+ 
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+int main()
+{
+	int tt;
+	cin>>tt;
+	while(tt--)
+	{
+		ll n;
+		cin>>n;
+		set<ll>s;
+		for(ll i=n+1;i>0;i--)
+		{
+			ll ans = floor(n/i);
+			s.insert(ans);
+		}
+		cout<<s.size()<<endl;
+		for(auto it=s.begin();it!=s.end();++it)
+		cout<<*it<<" ";
+		next;
+	}
+  return 0;
+}
