b2d18158aecb7443a794a29d67db3c0aa708f4cc
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-13 14:29:53 +0530

    Code Template for Google Code Jam

---
+++ Code_Templates/Google.py
@@ -0,0 +1,10 @@
+
+f = open('sample-input.txt')
+o = open('sample-output.txt', 'w')
+t = int(f.readline().strip())
+for i in xrange(1, t + 1):
+    o.write("Case #{}: ".format(i))
+    n = int(f.readline().strip())
+    x = [int(j) for j in f.readline().strip().split()]
+    y = [int(j) for j in f.readline().strip().split()]
+    o.write("\n")
