74fef59501d271eadfe4f19a79c1019d6be59928
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-13 14:28:09 +0530

    Code Template in Codeforces

---
+++ Code_Templates/Codeforces.cpp
@@ -0,0 +1,61 @@
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░
+// ░░░░░░▀░░░░░░░░░░░░░░░░░░▀░░░░░
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<long long int> vll;
+typedef vector<char> vc;
+typedef long double ld;
+
+typedef set<int>::iterator sit;
+typedef map<int,int>::iterator mit;
+typedef vector<int>::iterator vit;
+typedef vector<long long int>::iterator vllit;
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define      each(it,s)        for(vit it = s.begin(); it != s.end(); ++it)
+#define      sortA(v)          sort(v.begin(), v.end())
+#define      sortD(v)          sort(v.begin(), v.end(), greater<auto>())
+#define      fill(a)           memset(a, 0, sizeof (a))
+
+#define next cout<<endl
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(long long int i=1;i<n;++i)
+#define rf(j,n) for(long long int j=n-2;j>=0;--j)
+#define pq(x)		priority_queue<x,std::vector<x>,compare>
+#define rpq(x)	priority_queue<x,std::vector<x>,compare>
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+#define fbo find_by_order
+#define ook order_of_key
+
+/*
+ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    */  
