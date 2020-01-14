f0e2c0b384980c3562c58bfbeeda06285331e4c8
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-14 22:24:17 +0530

    Educational Codeforces Round 80, problem: (A)

---
+++ codeforces/deadline.cpp
@@ -0,0 +1,54 @@
+// contest: Educational Codeforces Round 80 (Rated for Div. 2), problem: (A) Deadline, Accepted, #, hack it!
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+ 
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define f2(i,n) for(int i=2;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>0;j--)
+#define ll long long int
+#define next cout<<endl
+ 
+using namespace std;
+ 
+ 
+int max(int a,int b)
+{
+    int ans = (a>=b)?a:b;
+    return ans;
+}
+ 
+int min(int a,int b)
+{
+    int ans = (a<=b)?a:b;
+    return ans;
+}
+ 
+int main() 
+{	
+	int t;
+    cin>>t;
+	while(t--)
+    {
+        ll n,d,ans=0;
+        cin>>n>>d;
+        if((n+1)*(n+1)- (4*d)>=0)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;   
+	}
+    return 0;	
+}
