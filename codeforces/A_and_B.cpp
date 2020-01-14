0cc8d859e2e4190d0dba8407123620442a515f4f
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-14 16:33:41 +0530

    Educational Codeforces Round 78 : problem: (B)

---
+++ codeforces/A_and_B.cpp
@@ -0,0 +1,55 @@
+// contest: Educational Codeforces Round 78 (Rated for Div. 2), problem: (B) A and B, Accepted, #
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+ 
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define f2(i,n) for(int i=2;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>0;j--)
+#define ll long long int
+#define next cout<<endl
+ 
+using namespace std;
+ 
+ 
+int max(int a,int b)
+{
+    int ans = (a>=b)?a:b;
+    return ans;
+}
+ 
+int min(int a,int b)
+{
+    int ans = (a<=b)?a:b;
+    return ans;
+}
+ 
+int main() 
+{	
+	int t;
+    cin>>t;
+	while(t--)
+    {
+        ll a,b,i;
+        cin>>a>>b;
+        a=abs(a-b);
+        b=i=0;
+        while(a>b||(b-a)%2!=0)
+            b+=++i;
+        cout<<i<<endl;
+	}
+    return 0;	
+}
