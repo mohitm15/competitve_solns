8026f916538b0e1f0bec44fb0f9020a3be63e906
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-17 20:04:06 +0530

    Codeforces Round 613 (Div. 2) ,Problem (C)

---
+++ codeforces/Faddi_and_LCM.cpp
@@ -0,0 +1,52 @@
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+ 
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define f2(i,n) for(int i=2;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>=0;j--)
+#define ll long long int
+#define next cout<<endl
+ 
+using namespace std;
+ 
+ 
+int max(int a,int b)
+{
+    int ans = (a>=b)?a:b;
+    return ans;
+}
+ 
+int min(int a,int b)
+{
+    int ans = (a<=b)?a:b;
+    return ans;
+}
+ 
+int main() 
+{	
+	ll x;
+	cin>>x;
+	for(ll n = sqrt(x);n>0;n--)
+	{
+		if(x%n==0 && (__gcd(n,x/n)==1))
+		{
+			cout<<n<<" "<<x/n<<endl;
+			break;
+		}
+	}
+    return 0;	
+}
