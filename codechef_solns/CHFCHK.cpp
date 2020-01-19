02aa9cbd5e113583bfe435f758c9ef3ccc4592f0
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2020-01-19 22:27:44 +0530

    Codechef January Cook-Off 2020 : Problem 1

---
+++ codechef_solns/CHFCHK.cpp
@@ -0,0 +1,55 @@
+// Codechef January Cook-Off 2020 Division 2 : Problem 1
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// ░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄░░░░░░
+// ░░░░░░█░░▄▀▀▀▀▀▀▀▀▀▀▀▀▄░░█░░░░░
+// ░░░░░░█░█░░▀░░░░░░░░▀░░█░█░░░░░
+// ░░░░░░█░█░░░▀░░░░░░▀░░░█░█░░░░░
+// ░░░░░░█░█░░░░▀░░░░▀░░░░█░█░░░░░
+// ░░░░░░█░█▄░░░░▀░░▀░░░░▄█░█░░░░░
+// ░░░░░░█░█░░░░░░██░░░░░░█░█░░░░░
+// ░░░░░░█░▀░░░░░░░░░░░░░░▀░█░░░░░
+// ░░░░░░█░░░░░░  ░░░░░  ░░░█░░░░░
+// ░░░░░░█░░░░░░ ░ ░░░ ░ ░░░█░░░░░
+// ░░░░░░▀░░░░░░ ░░ ░ ░░ ░░░▀░░░░░
+// ░░░░░░░░░░░░░ ░░░ ░░░ ░░░░░░░░░
+ 
+#include<bits/stdc++.h>
+
+#define f0(i,n) for(long long int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<=n;i++)
+#define f2(i,n) for(int i=2;i<=n;i++)
+#define rf(j,n) for(int j=n-1;j>0;j--)
+#define ll long long int
+#define next cout<<endl
+ 
+using namespace std;
+
+
+int max(int a,int b)
+{
+    int ans = (a>=b)?a:b;
+    return ans;
+}
+
+int min(int a,int b)
+{
+    int ans = (a<=b)?a:b;
+    return ans;
+}
+
+ int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        f0(i,n) cin>>a[i];
+        int ans = *std::min_element(a.begin(),a.end());
+        cout<<ans<<endl;
+    }
+    return 0;
+}
